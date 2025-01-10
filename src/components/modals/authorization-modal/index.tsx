c95c6ba731833be0d475a929094c5d251716b709
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2025-01-10 17:50:07 +0500

    login page Tugadi

--- src/components/modals/authorization-modal/index.tsx
+++ src/components/modals/authorization-modal/index.tsx
@@ -1,25 +1,45 @@
 import { Modal } from "antd";
 import { useReduxDispatch, useReduxSelector } from "../../../hooks/useRedux";
 import { setAuthoriationModalVisibilaty } from "../../../redux/modal-clise";
+import Login from "./login";
+import Register from "./register";
+import { useState } from "react";
 
 function AuthorizationModal() {
+  const [login, setLogin] = useState<boolean>(true);
+
   const dispatch = useReduxDispatch();
   const { authoriationModalVisibilaty } = useReduxSelector(
     (state) => state.modalSlice
   );
   return (
-    <div>
-      <Modal
-        open={authoriationModalVisibilaty}
-        onCancel={() => dispatch(setAuthoriationModalVisibilaty())}
-        footer={false}
-      >
-        <p>Salom</p>
-        <p>Salom</p>
-        <p>Salom</p>
-        <p>Salom</p>
-      </Modal>
-    </div>
+    <Modal
+      open={authoriationModalVisibilaty}
+      onCancel={() => dispatch(setAuthoriationModalVisibilaty())}
+      footer={false}
+    >
+      <div className="flex items-center justify-center gap-9">
+        <h3
+          onClick={() => setLogin(true)}
+          className={` text-[19px] cursor-pointer font-medium ${
+            login ? "text-green-600" : "text-[#000] "
+          }`}
+        >
+          Login
+        </h3>
+        <span className=" h-[20px] border border-[#000] "></span>
+        <h3
+          onClick={() => setLogin(false)}
+          className={` text-[19px] cursor-pointer font-medium ${
+            login ? "text-[#000] " : "text-green-600"
+          }`}
+        >
+          Register
+        </h3>
+      </div>
+
+      {login ? <Login /> : <Register />}
+    </Modal>
   );
 }
 
--- src/components/modals/authorization-modal/login/index.tsx
+++ src/components/modals/authorization-modal/login/index.tsx
@@ -1,5 +1,66 @@
+import { Form, Input } from "antd";
+import { FieldType } from "../../../../@types";
+import google from "../../../../img/google.svg";
+import facebook from "../../../../img/facebook.svg";
 function Login() {
-  return <div>Login</div>;
+  const onFinish = (e: any) => {
+    console.log(e);
+  };
+
+  return (
+    <div className="">
+      <p className="py-4 text-center text-[14px] text-[#3D3D3D]">
+        Enter your username and password to login.
+      </p>
+      <Form
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType>
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input
+            className="border-[#eaeaea] h-[40px] hover:border-[#46A358] focus:border-[#46A358]"
+            placeholder="almamun_uxui@outlook.com"
+          />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password
+            className="border-[#eaeaea] h-[40px] hover:border-[#46A358] focus:border-[#46A358]"
+            placeholder="***********"
+          />
+        </Form.Item>
+
+        <p className="text-end text-[#46a358] text-[15px]">Forgot Password? </p>
+        <button
+          className={`bg-[#46a358] w-full h-[40px] rounded-md text-white mt-5 text-[18px] `}
+          type="submit"
+        >
+          Login
+        </button>
+      </Form>
+
+      <div className="flex justify-center items-center flex-col gap-[20px] mt-5">
+        <div className="w-[100%] flex items-center justify-center mt-5 mb-5 gap-4">
+          <div className="w-[30%] h-[2px] bg-[#EAEAEA]"></div>
+          <p className="w-[40%]text-[#3D3D3D] text-[13px]">Or login with</p>
+          <div className="w-[30%] h-[2px] bg-[#EAEAEA]"></div>
+        </div>
+        <div className="border w-full h-[40px] rounded-md flex items-center justify-center gap-3 mb-4 cursor-pointer">
+          <img src={google} alt="" />
+        </div>
+        <div className="border w-full h-[40px] rounded-md flex items-center justify-center gap-3 mb-4 cursor-pointer">
+          <img src={facebook} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
